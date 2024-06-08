aa524453d549121002166746e500913bd9fd9d77
Muhammad Waseem <mw11221122@gmail.com>
2024-06-08 04:58:11 -0700

    Second decorator is completed

--- decor.py
+++ decor.py
@@ -1,3 +1,12 @@
+def strn_decor(func):
+    def wrapper(**kwargs):
+        result = func(kwargs)
+        return result
+
+    return wrapper
+
+
+
 def hello_world(func):
     print('Hi!')
     def wrapper(*args):
@@ -6,6 +15,12 @@ def hello_world(func):
         return 'Decorator is complete!'
     
     return wrapper
+@strn_decor
+def greetings(greetin):
+    print(type(greetin))
+    for a, b in greetin.items():
+        result = b
+    return result
 
 
 @hello_world
@@ -13,3 +28,4 @@ def hello(a, b):
     return a+b
 
 print(hello(2,3))
+print(greetings(greets = 'Hello Everyone! How are you doing?'))
