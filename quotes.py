b887b09486ab737dda383a8c8cdf256c288b1e75
Muhammad Waseem <mw11221122@gmail.com>
2025-02-12 06:43:38 -0800

    Pagination is complete

--- quotes.py
+++ quotes.py
@@ -6,14 +6,27 @@ class QuotesToScrap():
 
 #A Constructor in python
     def __init__(self): 
-        self.res = requests.get('https://quotes.toscrape.com/')
-        self.soup = BeautifulSoup(self.res.content, 'html.parser')
+        self.quotes_list = []
+        self.authors_list = []
+        self.tags_list = []
 
 #This Function scrapes the data from quotesToScrape website
-    def scrap_data(self): 
-        self.quotes = self.soup.find_all('span', class_ = 'text')
-        print(self.quotes[0].text)
+    def scrap_data(self):
+        for i in range(1,11):
+            self.res = requests.get(f'https://quotes.toscrape.com/page/{i}/')
+            self.soup = BeautifulSoup(self.res.content, 'html.parser')
+
+            self.quotes = self.soup.find_all('span', class_ = 'text')
+            self.author = self.soup.find_all('small', class_ = 'author')
+            self.tags = self.soup.find_all('div', class_ = 'tags')
+            
+            for a,b,c in zip(self.quotes, self.author, self.tags):
+                self.quotes_list.append(a.text)
+                self.authors_list.append(b.text)
+                self.tags_list.append(b.text)
+        
 
 
 obj1 = QuotesToScrap()
 obj1.scrap_data()
+print(obj1.quotes_list)
